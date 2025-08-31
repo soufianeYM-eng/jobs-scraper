74bc624ed229db7d3d61ba2c24959f1e0846bbee
SufianYM <yousfisoufiane8@gmail.com>
2025-09-01 00:39:01 +0200

    refactor(CR): resolve code review comments

--- src/telegram/telegram.update.ts
+++ src/telegram/telegram.update.ts
@@ -6,14 +6,7 @@ import { Context, Telegraf } from 'telegraf';
 @Injectable()
 @Update()
 export class TelegramUpdate {
-  private readonly botToken: string;
-
-  constructor(
-    @InjectBot() private readonly bot: Telegraf<Context>,
-    private readonly configService: ConfigService,
-  ) {
-    this.botToken = configService.get<string>('TELEGRAM_BOT_TOKEN') ?? '';
-  }
+  constructor(@InjectBot() private readonly bot: Telegraf<Context>) {}
 
   @Start()
   async onStart(@Ctx() ctx: Context) {
