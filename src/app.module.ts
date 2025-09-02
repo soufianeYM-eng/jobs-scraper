97ee33520a0c6952c3dc3766cd56af0fcd2ec6f4
SufianYM <yousfisoufiane8@gmail.com>
2025-09-02 23:55:18 +0200

    feature(User): generate User resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { JobsScraperModule } from './jobs-scraper/jobs-scraper.module';
 import { TelegramModule } from './telegram/telegram.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { TelegramModule } from './telegram/telegram.module';
     }),
     JobsScraperModule,
     TelegramModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
